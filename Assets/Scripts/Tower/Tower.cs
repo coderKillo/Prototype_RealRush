6aa421531f3df264206d8245f3f1aa6cce4a7e6a
coderKillo <coderKillo@users.noreply.github.com>
2022-05-21 09:34:17 +0200

    add: build time for tower

--- Assets/Scripts/Tower/Tower.cs
+++ Assets/Scripts/Tower/Tower.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Tower : MonoBehaviour
 {
     [SerializeField] private int cost = 5;
+    [SerializeField] private float buildTime = 1f;
 
     public bool CreateTower(GameObject prefab, Vector3 position)
     {
@@ -17,6 +18,34 @@ public class Tower : MonoBehaviour
 
         bank.WithDraw(cost);
         GameObject.Instantiate(prefab, position, Quaternion.identity);
+
         return true;
+    }
+
+    private void Start()
+    {
+        StartCoroutine(Build());
+    }
+
+    IEnumerator Build()
+    {
+        foreach (Transform child in transform)
+        {
+            child.gameObject.SetActive(false);
+            foreach (Transform grandchild in child)
+            {
+                grandchild.gameObject.SetActive(false);
+            }
+        }
+
+        foreach (Transform child in transform)
+        {
+            yield return new WaitForSeconds(buildTime / transform.childCount);
+            child.gameObject.SetActive(true);
+            foreach (Transform grandchild in child)
+            {
+                grandchild.gameObject.SetActive(true);
+            }
+        }
     }
 }
